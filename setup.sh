9423e58c8b04b8d56d66bdd192f103e5ff8de67c
franaln <franaln@gmail.com>
2015-08-29 00:17:12 +0200

    update setup.sh to last ST

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+
 localSetupDQ2Client --skipConfirm
 localSetupFAX --skipConfirm
 localSetupPandaClient currentJedi --noAthenaCheck
-rcSetup Base,2.3.12
-# rc find_packages
-# rc compile
+
+rcSetup SUSY,2.3.23
