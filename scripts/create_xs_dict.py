d40da85fe979b412c1c66916da29d0c76fd3e666
franaln <franaln@gmail.com>
2016-07-19 09:05:26 +0200

    update XS script

--- scripts/create_xs_dict.py
+++ scripts/create_xs_dict.py
@@ -6,14 +6,18 @@ import sys
 #xs_file = os.environ['ROOTCOREDIR'] + '/data/SUSYTools/susy_crosssections_13TeV.txt'
 try:
     xs_file = sys.argv[1]
-    sample_file = sys.argv[2]
+    sample_files = [ i for i in sys.argv[2:] ]
 except:
-    print 'usage: create_xs_dict.py [xs_file] [sample_file]'
+    print 'usage: create_xs_dict.py [xs-file] [sample-file] [sample-file2] ...'
     sys.exit(1)
 
 
 xs_lines = open(xs_file).read().split('\n')
-sample_lines = open(sample_file).read().split('\n')
+
+slines = []
+for sfile in sample_files:
+    slines.extend(open(sfile).read().split('\n'))
+
 
 def get_xs(did):
     
@@ -31,19 +35,20 @@ def get_xs(did):
 
 
 
-with open('xs_dict.py', 'w+') as of:
+xs_dict = dict()
 
-    of.write('xs_dict = dict()\n\n')
+for line in slines:
 
-    for line in sample_lines:
+    if not line or line.startswith('#'):
+        continue
 
-        if not line or line.startswith('#'):
-            continue
+    did = line.split('.')[1]
 
-        did = line.split('.')[1]
+    xs_dict[did] = get_xs(did)
 
-        xs = get_xs(did)
 
-        print 'xs for %s: %s' % (did, xs)
-        of.write('xs_dict[%s] = %s\n' % (did, xs))
+
+for did, xs in sorted(xs_dict.iteritems()):
+
+    print 'xs_dict[%s] = %s' % (did, xs)
 
