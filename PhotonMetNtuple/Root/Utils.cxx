15d065fc1f2aff7b9e6596f6b8f497bc45467560
Francisco Alonso <francisco.alonso@cern.ch>
2016-12-22 20:20:50 +0100

    fix dphi def in Utils.cxx

--- PhotonMetNtuple/Root/Utils.cxx
+++ PhotonMetNtuple/Root/Utils.cxx
@@ -6,5 +6,7 @@
 
 double get_dphi(Double_t phi1, Double_t phi2)
 {
-  return TVector2::Phi_mpi_pi(phi1 - phi2);
+  double  phi = fabs(phi1 - phi2);
+  if (phi <= TMath::Pi())  return phi;
+  else                     return (2 * TMath::Pi() - phi); 
 }
